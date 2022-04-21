541ca718911373b2951924cbea0979dc887c4c82
Simon Moulin <simonmoulin.pro@gmail.com>
2022-04-21 23:13:11 +0200

    Validation with dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthSevice } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthSevice) { }
 
     @Post('/signup')
-    signup() {
+    signup(@Body() dto: AuthDto) {
+        console.log({ dto })
         return this.authService.signup()
     }
 
