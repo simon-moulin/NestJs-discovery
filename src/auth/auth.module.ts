96f5ba574aab18fca7f9bba969e546f6076fdcf5
Simon Moulin <simonmoulin.pro@gmail.com>
2022-04-21 21:53:40 +0200

    First controller & service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthSevice } from "./auth.service";
 
-@Module({})
+@Module({
+    controllers: [AuthController],
+    providers: [AuthSevice]
+})
 export class AuthModule {
-
+    
 }
